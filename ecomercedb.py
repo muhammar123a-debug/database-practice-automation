bde6ad918a35416c21ebcd0de817a04500b2032c
muhammar123a-debug <muhammar123a@gmail.com>
2025-09-03 18:25:01 +0500

    fix: OpenRouter auth setup

--- ecomercedb.py
+++ ecomercedb.py
@@ -8,9 +8,12 @@ conn = mysql.connector.connect(
 
 cursor = conn.cursor()
 
+cursor.execute("DROP DATABASE the_mart;")
+
 # Create Database
 cursor.execute("CREATE DATABASE IF NOT EXISTS the_mart")
 cursor.execute("USE the_mart")
+
 
 # Customers Table
 cursor.execute("""
@@ -56,5 +59,26 @@ CREATE TABLE IF NOT EXISTS order_items(
     FOREIGN KEY (product_id) REFERENCES products(id)
 )
 """)
+cursor.execute("INSERT INTO customers (name, email, phone, city) VALUES ('Ammar','ammar@gmail.com','03202595322','canada'),('Anas','anas@gmail.com','03221254664','karachi'),('Asim','asim@gmail.com','03220032030','Lahore')")
+cursor.execute("SELECT * FROM customers")
+for i in cursor.fetchall():
+    print(i)
 
-print("Tables created successfully ✅")
+cursor.execute("INSERT INTO products (product_name, price, stock) VALUES ('Laptop', 120000, 10),('Mobile', 45000, 20),('Headphones', 3000, 50),('Keyboard', 1500, 30);")
+cursor.execute("SELECT * FROM products")
+for i in cursor.fetchall():
+    print(i)
+
+cursor.execute("INSERT INTO orders (customer_id, status) VALUES (1, 'Pending'),(2, 'Shipped'),(3,'Delivered')")
+cursor.execute("SELECT * FROM orders")
+for i in cursor.fetchall():
+    print(i)
+    
+cursor.execute("INSERT INTO order_items (order_id,product_id, quantity,price) VALUES (1,1,1, 120000),(1,3,2, 6000)")
+cursor.execute("SELECT * FROM order_items")
+for i in cursor.fetchall():
+    print(i)
+
+conn.commit()
+
+print("Sample customers & products added ✅")
