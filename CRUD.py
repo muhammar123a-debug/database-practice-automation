67217fb9279c03effb9c0f34f55dcee26fb10410
muhammar123a-debug <muhammar123a@gmail.com>
2025-09-04 20:21:44 +0500

    Add more practice queries

--- CRUD.py
+++ CRUD.py
@@ -28,4 +28,46 @@ rows = [
     ("Qasim Khan", "qasimkhan@gmail.com","03221234567","Peshawer")
 ]
 cursor.executemany("INSERT INTO customers (name, email, phone, city) VALUES (%s,%s,%s,%s)",rows)
+
+#SELECT queries
+print("\n Acsess All Data from customers TABLE")
+cursor.execute("SELECT * FROM customers")
+for i in cursor.fetchall():
+    print(i)
+
+print("\n Select only specific columns (name, city)")
+cursor.execute("SELECT name, city FROM customers")
+for i in cursor.fetchall():
+    print(i)
+
+print("\n Select with WHERE condition (city = 'Karachi')")
+cursor.execute("SELECT * FROM customers WHERE city = 'Karachi'")
+for i in cursor.fetchall():
+    print(i)
+
+print("\n Order results by name ASC/DESC + LIMIT (top 5 customers)")
+cursor.execute("SELECT * FROM customers ORDER BY name ASC LIMIT 5")
+for i in cursor.fetchall():
+    print(i)
+print("\n Order results by name DESC")
+cursor.execute("SELECT * FROM customers ORDER BY name DESC LIMIT 3")
+for i in cursor.fetchall():
+    print(i)
+
+print("Update a single customers city")
+cursor.execute("UPDATE customers SET city = 'Karachi' WHERE id = 5")
+print("Update multiple fields at once (email + phone)")
+cursor.execute("UPDATE customers SET email = 'muhammar@gmail.com', phone = '03009998888' WHERE id = 2;")
+print("Update rows with condition (Karachi → Lahore)")
+cursor.execute("UPDATE customers SET city = 'Lahore' WHERE city = 'Karachi'")
+print("Update multiple rows at once (name LIKE 'A%' → city = 'Islamabad')")
+cursor.execute("UPDATE customers SET city = 'Islamabad' WHERE name LIKE 'A%'")
+print("Update using Python script (row selection with input)")
+id = int(input("Enter customer ID to update: "))
+new_city = input("Enter new city:")
+cursor.execute("UPDATE customers SET city = %s WHERE id = %s", (new_city, id))
+cursor.execute("SELECT * FROM customers")
+for i in cursor.fetchall():
+    print(i)
+
 connect.commit()
